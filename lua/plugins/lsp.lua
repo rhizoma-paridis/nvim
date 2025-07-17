6490e5950b3f7b3690fbd46640ca5c9e059c9029
rhizoma_paridis <rhizoma_paridis@outlook.com>
2025-07-17 18:27:33 +0800

    init

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,19 @@
+return {
+    {
+        "mason-org/mason.nvim",
+        opts = {}
+    },
+    {
+        'neovim/nvim-lspconfig',
+    },
+    {
+        "mason-org/mason-lspconfig.nvim",
+        opts = {
+            ensure_installed = { "lua_ls" },
+        },
+        dependencies = {
+            "mason-org/mason.nvim",
+            'neovim/nvim-lspconfig',
+        }
+    }
+}
