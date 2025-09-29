80ed0983f29fb9836c1e3387110fad64e3a803e8
rhizoma_paridis <rhizoma_paridis@outlook.com>
2025-09-29 16:27:14 +0800

    change snack dashboard

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -9,11 +9,10 @@ return {
             dashboard = {
                 preset = {
                     keys = {
-                        { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
                         { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
                         { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
                         { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-                        { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+                        { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
                         { icon = " ", key = "q", desc = "Quit", action = ":qa" },
                     },
                 },
