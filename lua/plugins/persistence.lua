3bdc135dd69f0386bbf167a358a22a14c4dd9fce
rhizoma_paridis <rhizoma_paridis@outlook.com>
2025-09-29 16:11:11 +0800

    remove auto-session

--- lua/plugins/persistence.lua
+++ lua/plugins/persistence.lua
@@ -2,12 +2,14 @@ return {
     {
         "folke/persistence.nvim",
         event = "BufReadPre",
-        enabled = false,
-        opts = {},
+        enabled = true,
+        opts = {
+            need = 1,
+            branch = true, 
+        },
         -- stylua: ignore
         keys = {
-            { "<leader>qs", function() require("persistence").load() end,                desc = "Restore Session" },
-            { "<leader>qS", function() require("persistence").select() end,              desc = "Select Session" },
+            { "<leader>qs", function() require("persistence").select() end,              desc = "Select Session" },
             { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
             { "<leader>qd", function() require("persistence").stop() end,                desc = "Don't Save Current Session" },
         },
