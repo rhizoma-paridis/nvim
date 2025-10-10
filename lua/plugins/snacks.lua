261a96d41d55fc037d4bb9a133364f8d156a5708
rhizoma_paridis <rhizoma_paridis@outlook.com>
2025-10-10 16:28:57 +0800

    recover snack lsp key

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -24,7 +24,7 @@ return {
                 }
             },
             explorer = { enabled = true },
-            indent = { 
+            indent = {
                 enabled = true,
                 indent = {
                     char = "┆",
@@ -121,7 +121,7 @@ return {
             { "<leader>cl", function() Snacks.picker.lsp_config() end,            desc = "Lsp Info" },
             { "gd",         function() Snacks.picker.lsp_definitions() end,       desc = "Goto Definition" },
             { "gD",         function() Snacks.picker.lsp_declarations() end,      desc = "Goto Declaration" },
-            -- { "gr",              function() Snacks.picker.lsp_references() end,                          nowait = true,                     desc = "References" },
+            { "gr",         function() Snacks.picker.lsp_references() end,        nowait = true,                     desc = "References" },
             { "gI",         function() Snacks.picker.lsp_implementations() end,   desc = "Goto Implementation" },
             { "gy",         function() Snacks.picker.lsp_type_definitions() end,  desc = "Goto T[y]pe Definition" },
             { "<leader>ss", function() Snacks.picker.lsp_symbols() end,           desc = "LSP Symbols" },
